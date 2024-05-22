2ff800af97d0da3b4a24bdb95f76eb4e6e4d876c
carmith <carmith@jfrog.com>
2024-05-22 15:44:33 +0300

    Using an old version of transformers

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
     author_email='jfrog@jfrog.com',
     url='https://github.com/carmithersh/carmit-testing',
     packages=['pythonExample'],
-    install_requires=['PyYAML>3.11', 'nltk'],
+    install_requires=['PyYAML>3.11', 'nltk', 'transformers'],
 )
