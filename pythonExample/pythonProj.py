451f9c5631c5b75f4ad5657f8ddc5c2d938ba8e3
carmith <carmith@jfrog.com>
2024-05-28 15:47:03 +0300

    Adding requests dependencies

--- pythonExample/pythonProj.py
+++ pythonExample/pythonProj.py
@@ -12,8 +12,8 @@ import hashlib
 
 def arc4_encrypt_password(key, password):
     cipher = ARC4.new(key.encode('utf-8'))
-    encrypted_password = cipher.encrypt(password.encode('utf-8'))
-    return hashlib.md5(encrypted_password).hexdigest()
+    encrypted_passcode = cipher.encrypt(password.encode('utf-8'))
+    return hashlib.md5(encrypted_passcode).hexdigest()
 
 # Now you can call printme function
 printme("Hello from JFROG");
